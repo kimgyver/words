c6355823b6fbc3540da3e302894b7d3bd08e3b78
unknown <jinyoung777@gmail.com>
2020-01-19 20:35:56 +1300

    netlify

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const serverUrl = () => {
   return process.env.NODE_ENV === 'development'
     ? 'http://localhost:9000/.netlify/functions/api'
-    : process.env.server_url;
+    : 'https://word-backend-jason.netlify.com/.netlify/functions/api';
 };
