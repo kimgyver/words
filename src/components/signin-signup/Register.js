744d53fd6fbd43f5fd114c2e03fb1753ec62589f
unknown <jinyoung777@gmail.com>
2020-02-05 21:40:31 +1300

    change call of register() to await util finished job

--- src/components/signin-signup/Register.js
+++ src/components/signin-signup/Register.js
@@ -35,7 +35,7 @@ const Register = ({
 
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (name === '' || email === '' || password === '') {
       M.toast({ html: 'Please enter all fields' });
@@ -44,7 +44,7 @@ const Register = ({
       M.toast({ html: 'Passwords do not match' });
       //setAlert('Passwords do not match', 'danger');
     } else {
-      register({
+      await register({
         name,
         email,
         password
