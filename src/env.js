abf239bb7c85df2477efb208e0b4d1396d451285
Jinyoung Kim <jinyoung777@gmail.com>
2020-05-01 15:14:59 +1200

    changed to heroku server

--- src/env.js
+++ src/env.js
@@ -1,5 +1,6 @@
 export const serverUrl = () => {
   return process.env.NODE_ENV === 'development'
     ? 'http://localhost:9000/.netlify/functions/api'
-    : 'https://word-backend-jason.netlify.com/.netlify/functions/api';
+    : "https://words-backend-jason.herokuapp.com/.netlify/functions/api";
+    //: 'https://word-backend-jason.netlify.com/.netlify/functions/api';
 };
